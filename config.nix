2b0a1a60e72b8f4d6c97227156c8738f5b9da64c
Moritz Angermann <moritz.angermann@gmail.com>
2018-03-05 21:37:13 +0800

    bunch of customizations.

--- config.nix
+++ config.nix
@@ -14,19 +14,42 @@ let customizeGhc = oldGhc:
    # patches = map fetchDiff ghcDiffs;
    hardeningDisable = [ "stackprotector" ];
  });
+
 in
 {
     packageOverrides = ps: rec {
+      libyaml = ps.libyaml.overrideAttrs (drv: { patches = [ ./libyaml.patch ]; });
+
       haskell.compiler = ps.haskell.compiler // {
         ghcHEAD = customizeGhc ps.haskell.compiler.ghcHEAD;
       };
-      haskell.packages = ps.haskell.packages // {
-        ghcHEAD = ps.haskell.packages.ghcHEAD.override {
-          ghc = customizeGhc ps.buildPackages.haskell.compiler.ghcHEAD;
-        };
-        # Cabal_HEAD = ps.buildPackages.haskell.packages.ghcHEAD.callPackage ./cabal-head.nix { };
+      haskell.packages = ps.haskell.packages //
+        (let addLibraryDepends = drv: xs: ps.haskell.lib.overrideCabal drv (drv: { libraryHaskellDepends = (drv.libraryHaskellDepends or []) ++ xs; });
+        in { ghcHEAD = ps.haskell.packages.ghcHEAD.override {
+          overrides = self: super: {
+      	    ghc = customizeGhc ps.buildPackages.haskell.compiler.ghcHEAD;
 
-      };
+            mkDerivation = drv: super.mkDerivation (drv // {
+              enableSharedLibraries = false; 
+              enableSharedExecutables = false;
+#              setupHaskellDepends = (drv.setupHaskellDepends or []) ++ [ self.Cabal_HEAD ];
+              hardeningDisable = [ "stackprotector" ];
+              doHaddock = false;
+            });
+            text = ps.haskell.lib.appendPatch super.text ./head.hackage/patches/text-1.2.2.2.patch;
+            blaze-builder = ps.haskell.lib.appendPatch super.blaze-builder ./head.hackage/patches/blaze-builder-0.4.0.2.patch;
+            unordered-containers = ps.haskell.lib.appendPatch super.unordered-containers ./head.hackage/patches/unordered-containers-0.2.8.0.patch;
+
+            cabal-doctest = ps.haskell.lib.appendPatch super.cabal-doctest ./cabal-doctest.patch;
+            system-fileio = ps.haskell.lib.appendPatch super.system-fileio ./system-fileio.patch;
+            th-abstraction = ps.haskell.lib.appendPatch super.th-abstraction ./th-abstraction.patch;
+
+            ansi-terminal = addLibraryDepends super.ansi-terminal [ self.base-compat self.containers ]; #self.Win32 ];
+            double-conversion = ps.haskell.lib.appendPatch super.double-conversion ./double-conversion.patch;
+#             Cabal_HEAD = self.callPackage ./cabal-head.nix { };
+         };
+       };
+      });
       # TODO: Inject Cabal_HAED into setupHaskellDepends by default.
       #
       # // ps.lib.optionalAttrs (ps.hostPlatform != ps.buildPlatform) {
---
+++ dhall-json.nix
@@ -0,0 +1,5 @@
+with import <nixpkgs> { crossSystem = (import <nixpkgs/lib>).systems.examples.mingwW64;
+                        config = import ./config.nix; };
+let Cabal_HEAD = buildPackages.haskell.packages.ghcHEAD.callPackage ./cabal-head.nix { };
+in pkgs.haskell.packages.ghcHEAD.callPackage ./default-dhall-json.nix { inherit Cabal_HEAD; }
+
