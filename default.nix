935f45a2a6c745df5510c3dfe0ec8c85d2fbefd9
Michael Bishop <michael.bishop@iohk.io>
2018-03-06 09:18:51 -0400

    re-arrange some of the nix files

--- default.nix
+++ default.nix
@@ -1,14 +1,20 @@
-{ mkDerivation, base, stdenv, Cabal_HEAD }:
-mkDerivation {
-  pname = "hs-hello";
-  version = "1.0.0";
-  src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  enableSharedExecutables = false;
-  setupHaskellDepends = [ Cabal_HEAD ];
-  executableHaskellDepends = [ base ];
-  license = stdenv.lib.licenses.bsd3;
+let
+  pkgsPath = import ./fetchNixpkgs.nix {
+    rev = "ff9275c2886a1d984bf9a20ef7871b65d85aa08f";
+    sha256 = "0c8nwx1ghl6p1z0wv8rf02f1zgvry66q3jn3xk74dw578avnsfvy";
+    sha256unpacked = "1mz15byiv0fb0iyjb245c0q613ipmklmbzx4wb74i8lvmfwa3cwr";
+    owner = "angerman";
+  };
+in
+  { pkgsPath' ? pkgsPath }:
 
-  hardeningDisable = [ "stackprotector" ];
+with import pkgsPath' {
+  crossSystem = (import <nixpkgs/lib>).systems.examples.mingwW64;
+  config = import ./config.nix;
+};
+let Cabal_HEAD = buildPackages.haskell.packages.ghcHEAD.callPackage ./cabal-head.nix { };
+in {
+  hello-world = pkgs.haskell.packages.ghcHEAD.callPackage ./hello-world.nix { inherit Cabal_HEAD; };
+  cross-ghc = haskell.packages.ghcHEAD.ghc;
+  inherit Cabal_HEAD;
 }
