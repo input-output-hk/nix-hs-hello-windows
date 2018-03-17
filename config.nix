573c6587dc1d3cb8ca5c480682fc3384b8753644
Moritz Angermann <moritz.angermann@gmail.com>
2018-03-17 14:42:50 +0800

    Cleanup `config.nix`.

--- config.nix
+++ config.nix
@@ -21,95 +21,105 @@ in
   packageOverrides = ps: rec {
     libyaml = ps.libyaml.overrideAttrs (drv: { patches = [ ./libyaml.patch ]; });
 
-    haskell.compiler = ps.haskell.compiler // {
-      ghcHEAD = customizeGhc ps.haskell.compiler.ghcHEAD;
-    };
-    haskell.packages = ps.haskell.packages //
-      (let
+    haskell = ps.haskell // {
+      compiler = ps.haskell.compiler // {
+        ghcHEAD = customizeGhc ps.haskell.compiler.ghcHEAD;
+      };
+      packages = ps.haskell.packages //
+        (let
       addLibraryDepends = drv: xs: ps.haskell.lib.overrideCabal drv (drv: { libraryHaskellDepends = (drv.libraryHaskellDepends or []) ++ xs; });
       Cabal_HEAD = ps.buildPackages.haskell.packages.ghcHEAD.callPackage ./cabal-head.nix { };
-    in {
-        ghcHEAD = ps.haskell.packages.ghcHEAD.override {
-          overrides = self: super: {
-      	    ghc = customizeGhc ps.buildPackages.haskell.compiler.ghcHEAD;
 
-            mkDerivation = drv: super.mkDerivation (drv // {
-              enableSharedLibraries = false; 
-              enableSharedExecutables = false;
-              setupHaskellDepends = (drv.setupHaskellDepends or []) ++
-                ps.lib.optionals (ps.hostPlatform != ps.buildPlatform) [ Cabal_HEAD ];
-              hardeningDisable = [ "stackprotector" ];
-            doHaddock = false;
-            dontStrip = true;
-            });
+      in {
+          ghcHEAD = ps.haskell.packages.ghcHEAD.override {
+            overrides = self: super: {
+      	      ghc = customizeGhc ps.buildPackages.haskell.compiler.ghcHEAD;
 
-          zlib = ps.haskell.lib.overrideCabal super.zlib (drv: {
-            version = "0.6.2";
-            sha256 = "1vbzf0awb6zb456xf48za1kl22018646cfzq4frvxgb9ay97vk0d";
-          });
+              # fetch a package candidate from hackage and return the cabal2nix expression.
+              hackageCandidate = name: ver: args: self.callCabal2nix name (fetchTarball "https://hackage.haskell.org/package/${name}-${ver}/candidate/${name}-${ver}.tar.gz") args;
 
-            lens = let l = ps.haskell.lib.overrideCabal super.lens (drv: {
-              version = "4.16";
-              sha256 = "16wz3s62zmnmis7xs9jahyc7b75090b96ayk98c3gvzmpg7bx54z";
-            }); in let l2 = ps.haskell.lib.appendPatch l ./lens.patch;
-            in ps.haskell.lib.overrideCabal l2 (drv: { doVerbose = true; });
+              mkDerivation = drv: super.mkDerivation (drv // {
+                enableSharedLibraries = false;
+                enableSharedExecutables = false;
+                setupHaskellDepends = (drv.setupHaskellDepends or []) ++
+                  ps.lib.optionals (ps.hostPlatform != ps.buildPlatform) [ Cabal_HEAD ];
+                hardeningDisable = [ "stackprotector" ];
+                doHaddock = false;
+                dontStrip = true;
+              });
+
+              libiserv = with haskell.lib; addExtraLibrary (enableCabalFlag (self.hackageCandidate "libiserv" "8.5" {}) "network") self.network;
+              iserv-proxy = self.hackageCandidate "iserv-proxy" "8.5" { libiserv = self.libiserv; };
+              # Why is `network` not properly propagated from `libiserv`?
+              remote-iserv = with haskell.lib; addExtraLibrary (self.hackageCandidate "remote-iserv" "8.5" { libiserv = self.libiserv; }) self.network;
 
 
-          # patches from head.hackage.
-          text = ps.haskell.lib.appendPatch super.text ./head.hackage/patches/text-1.2.2.2.patch;
-          blaze-builder = ps.haskell.lib.appendPatch super.blaze-builder ./head.hackage/patches/blaze-builder-0.4.0.2.patch;
-          unordered-containers = ps.haskell.lib.appendPatch super.unordered-containers ./head.hackage/patches/unordered-containers-0.2.8.0.patch;
-          semigroupoids = ps.haskell.lib.appendPatch super.semigroupoids ./head.hackage/patches/semigroupoids-5.2.1.patch;
-          free = ps.haskell.lib.appendPatch super.free ./head.hackage/patches/free-4.12.4.patch;
-          x509 = ps.haskell.lib.appendPatch super.x509 ./x509-1.7.2.patch;
-          x509-store = ps.haskell.lib.appendPatch super.x509-store ./head.hackage/patches/x509-store-1.6.5.patch;
-#              in let p = ps.haskell.lib.appendPatches l [
-#                #            ./head.hackage/patches/lens-4.15.4.patch
-#                ./lens.patch
-#              ]; in ps.haskell.lib.overrideCabal p (drv: { doVerbose = true; });
+              zlib = ps.haskell.lib.overrideCabal super.zlib (drv: {
+                version = "0.6.2";
+                sha256 = "1vbzf0awb6zb456xf48za1kl22018646cfzq4frvxgb9ay97vk0d";
+              });
 
-          tls = ps.haskell.lib.appendPatch super.tls ./head.hackage/patches/tls-1.4.0.patch;
+              lens = let l = ps.haskell.lib.overrideCabal super.lens (drv: {
+                version = "4.16";
+                sha256 = "16wz3s62zmnmis7xs9jahyc7b75090b96ayk98c3gvzmpg7bx54z";
+              }); in ps.haskell.lib.appendPatch l ./lens.patch;
 
-          # --allow-newer :-(
-          integer-logarithms = ps.haskell.lib.appendPatch super.integer-logarithms ./integer-logarithms.patch;
-          async = ps.haskell.lib.appendPatch super.async ./async.patch;
-          bifunctors = ps.haskell.lib.appendPatch super.bifunctors ./bifunctors.patch;
-          unliftio-core = ps.haskell.lib.appendPatch super.unliftio-core ./unliftio-core.patch;
-          cabal-doctest = ps.haskell.lib.appendPatch super.cabal-doctest ./cabal-doctest.patch;
-          th-abstraction = ps.haskell.lib.appendPatch super.th-abstraction ./th-abstraction.patch;
 
-          #other patches
-            system-fileio = ps.haskell.lib.appendPatch super.system-fileio ./system-fileio.patch;
-            text-format = ps.haskell.lib.appendPatch super.text-format ./text-format-0.3.1.1.patch;
-            x509-system = ps.haskell.lib.appendPatch super.x509-system ./x509-system-1.6.6.patch;
-            streaming-commons = ps.haskell.lib.appendPatch super.streaming-commons ./streaming-commons-0.1.19.patch;
-            double-conversion = let p2 = (let p = ps.haskell.lib.appendPatch super.double-conversion ./double-conversion.patch;
+              # patches from head.hackage.
+              text = ps.haskell.lib.appendPatch super.text ./head.hackage/patches/text-1.2.2.2.patch;
+              blaze-builder = ps.haskell.lib.appendPatch super.blaze-builder ./head.hackage/patches/blaze-builder-0.4.0.2.patch;
+              unordered-containers = ps.haskell.lib.appendPatch super.unordered-containers ./head.hackage/patches/unordered-containers-0.2.8.0.patch;
+              semigroupoids = ps.haskell.lib.appendPatch super.semigroupoids ./head.hackage/patches/semigroupoids-5.2.1.patch;
+              free = ps.haskell.lib.appendPatch super.free ./head.hackage/patches/free-4.12.4.patch;
+              x509 = ps.haskell.lib.appendPatch super.x509 ./x509-1.7.2.patch;
+              x509-store = ps.haskell.lib.appendPatch super.x509-store ./head.hackage/patches/x509-store-1.6.5.patch;
+              #              in let p = ps.haskell.lib.appendPatches l [
+              #                #            ./head.hackage/patches/lens-4.15.4.patch
+              #                ./lens.patch
+              #              ]; in ps.haskell.lib.overrideCabal p (drv: { doVerbose = true; });
 
-            in ps.haskell.lib.overrideCabal p (drv: { doVerbose = true; }));
+              tls = ps.haskell.lib.appendPatch super.tls ./head.hackage/patches/tls-1.4.0.patch;
+
+              # --allow-newer :-(
+              integer-logarithms = ps.haskell.lib.appendPatch super.integer-logarithms ./integer-logarithms.patch;
+              async = ps.haskell.lib.appendPatch super.async ./async.patch;
+              bifunctors = ps.haskell.lib.appendPatch super.bifunctors ./bifunctors.patch;
+              unliftio-core = ps.haskell.lib.appendPatch super.unliftio-core ./unliftio-core.patch;
+              cabal-doctest = ps.haskell.lib.appendPatch super.cabal-doctest ./cabal-doctest.patch;
+              th-abstraction = ps.haskell.lib.appendPatch super.th-abstraction ./th-abstraction.patch;
+
+              #other patches
+              system-fileio = ps.haskell.lib.appendPatch super.system-fileio ./system-fileio.patch;
+              text-format = ps.haskell.lib.appendPatch super.text-format ./text-format-0.3.1.1.patch;
+              x509-system = ps.haskell.lib.appendPatch super.x509-system ./x509-system-1.6.6.patch;
+              streaming-commons = ps.haskell.lib.appendPatch super.streaming-commons ./streaming-commons-0.1.19.patch;
+              double-conversion = let p2 = (let p = ps.haskell.lib.appendPatch super.double-conversion ./double-conversion.patch;
+
+              in ps.haskell.lib.overrideCabal p (drv: { doVerbose = true; }));
             in ps.haskell.lib.appendConfigureFlag p2 [ "-v0" ];
-          StateVar = ps.haskell.lib.appendPatch super.StateVar ./StateVar.patch;
+              StateVar = ps.haskell.lib.appendPatch super.StateVar ./StateVar.patch;
 
-          mtl = ps.haskell.lib.overrideCabal super.mtl (drv: { libraryHaskellDepends = [ self.base self.transformers ]; });
+              mtl = ps.haskell.lib.overrideCabal super.mtl (drv: { libraryHaskellDepends = [ self.base self.transformers ]; });
 
-          contravariant = ps.haskell.lib.appendPatch super.contravariant ./contravariant-1.4.1.patch;
-          # missing libraries, due to os(mingw32) check not executed in cabal2nix
-          http-client = addLibraryDepends super.http-client [ self.safe ]; # self.Win32 ];
-          ansi-terminal = addLibraryDepends super.ansi-terminal [ self.base-compat self.containers ]; #self.Win32 ];
-          # disable integer-gmp on cryptonite
-          cryptonite = ps.haskell.lib.appendConfigureFlag super.cryptonite [ "-f-integer-gmp" ];
+              contravariant = ps.haskell.lib.appendPatch super.contravariant ./contravariant-1.4.1.patch;
+              # missing libraries, due to os(mingw32) check not executed in cabal2nix
+              http-client = addLibraryDepends super.http-client [ self.safe ]; # self.Win32 ];
+              ansi-terminal = addLibraryDepends super.ansi-terminal [ self.base-compat self.containers ]; #self.Win32 ];
+              # disable integer-gmp on cryptonite
+              cryptonite = ps.haskell.lib.appendConfigureFlag super.cryptonite [ "-f-integer-gmp" ];
 
-          # disable executables where needed
-          aeson-pretty = ps.haskell.lib.overrideCabal super.aeson-pretty (drv: { enableSharedExecutables = false; doVerbose = true; });
-          yaml = let p = ps.haskell.lib.overrideCabal         super.yaml         (drv: { enableSharedExecutables = false; }); in ps.haskell.lib.dontStrip p;
+              # disable executables where needed
+              aeson-pretty = ps.haskell.lib.overrideCabal super.aeson-pretty (drv: { enableSharedExecutables = false; doVerbose = true; });
+              yaml = let p = ps.haskell.lib.overrideCabal         super.yaml         (drv: { enableSharedExecutables = false; }); in ps.haskell.lib.dontStrip p;
 
 
-          # no stripping
-          attoparsec = ps.haskell.lib.dontStrip super.attoparsec;
-          aeson = ps.haskell.lib.dontStrip super.aeson;
-          #             Cabal_HEAD = self.callPackage ./cabal-head.nix { };
-         };
-       };
-      });
+              # no stripping
+              attoparsec = ps.haskell.lib.dontStrip super.attoparsec;
+              aeson = ps.haskell.lib.dontStrip super.aeson;
+              #             Cabal_HEAD = self.callPackage ./cabal-head.nix { };
+            };
+          };
+        });
       # TODO: Inject Cabal_HAED into setupHaskellDepends by default.
       #
       # // ps.lib.optionalAttrs (ps.hostPlatform != ps.buildPlatform) {
@@ -119,5 +129,6 @@ in
       #     ];
       #   });
       # };
+    };
     };
 }
