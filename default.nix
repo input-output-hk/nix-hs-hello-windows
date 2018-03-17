a6c6e07aea34a843c074f64d338e31b37ab10df9
Moritz Angermann <moritz.angermann@gmail.com>
2018-03-17 14:43:53 +0800

    More targets.

--- default.nix
+++ default.nix
@@ -12,13 +12,18 @@ with import nixpkgsPath {
   crossSystem = (import <nixpkgs/lib>).systems.examples.mingwW64;
   config = import ./config.nix;
 };
-let Cabal_HEAD = buildPackages.haskell.packages.ghcHEAD.callPackage ./cabal-head.nix { };
+let
+    Cabal_HEAD = buildPackages.haskell.packages.ghcHEAD.callPackage ./cabal-head.nix { };
+    iserv-proxy = buildPackages.haskell.packages.ghcHEAD.iserv-proxy;
 in {
   hello-world = pkgs.haskell.packages.ghcHEAD.callPackage ./hello-world.nix { inherit Cabal_HEAD; };
   dhall-json = pkgs.haskell.packages.ghcHEAD.callPackage ./dhall-json.nix { inherit Cabal_HEAD; };
   cross-ghc = haskell.packages.ghcHEAD.ghc;
   lens = haskell.packages.ghcHEAD.lens;
   double-conversion = haskell.packages.ghcHEAD.double-conversion;
+  libiserv = haskell.packages.ghcHEAD.libiserv;
+  remote-iserv = haskell.packages.ghcHEAD.remote-iserv;
   inherit Cabal_HEAD;
+  inherit iserv-proxy;
   inherit pkgs;
 }
